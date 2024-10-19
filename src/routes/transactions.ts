aaf43dbfc1a6b04e6e22af4cee3145a976f17e40
Matheus <matheus@matheusferreira.dev>
2024-10-19 16:49:30 -0300

    feat: add list and find transaction features

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,29 @@ import { z } from 'zod';
 import { knex } from '../database';
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select();
+
+    return { transactions };
+  });
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const params = getTransactionParamsSchema.parse(request.params);
+
+    const transaction = await knex('transactions')
+      .select()
+      .where({
+        id: params.id,
+      })
+      .first();
+
+    return { transaction };
+  });
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
